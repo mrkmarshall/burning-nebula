f11292da8eeecd5b7380bca7baaab68c1f25446a
mrkmarshall <michaelrkmarshall@gmail.com>
2021-09-08 16:24:12 -0700

    hope I'm doing this right lol

---
+++ SpaceTradersAPIClient.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace SpaceTradersAPIClient
+{
+    class Program
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        static async Task Main(string[] args)
+        {
+            await CallAPI();
+        }
+        
+        private static async Task CallAPI()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            var stringTask = client.GetStringAsync("https://api.spacetraders.io/game/status");
+
+            var msg = await stringTask;
+            Console.Write(msg);
+        }
+    }
+}
